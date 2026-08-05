27b21b9c4ebd9fb606c69d3bd72b72ce01501823
IRISTZU <mlive4h@gmail.com>
2026-08-05 15:30:12 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("LINE訂餐整理工具")
+
+uploaded_files = st.file_uploader(
+    "上傳 LINE 截圖",
+    accept_multiple_files=True
+)
+
+if uploaded_files:
+    st.success(
+        f"已上傳 {len(uploaded_files)} 張圖片"
+    )
+
+    for file in uploaded_files:
+        st.write(file.name)

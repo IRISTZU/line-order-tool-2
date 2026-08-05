6e415c3a141065dedca0a44da9612225b5035f73
IRISTZU <mlive4h@gmail.com>
2026-08-05 16:09:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ st.title("LINE訂餐整理工具")
 
 uploaded_files = st.file_uploader(
     "上傳 LINE 截圖",
+    type=["jpg", "jpeg", "png"],
     accept_multiple_files=True
 )
 
 if uploaded_files:
-    st.success(f"已上傳 {len(uploaded_files)} 張圖片")
+
+    st.success(
+        f"已上傳 {len(uploaded_files)} 張圖片"
+    )
+
+    st.subheader("檔案列表")
+
+    for file in uploaded_files:
+        st.write(file.name)

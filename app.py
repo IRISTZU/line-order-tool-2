8e4da81919d7d26b77d777a27e6c39c684c0e2cc
IRISTZU <mlive4h@gmail.com>
2026-08-05 16:03:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,30 @@
 import streamlit as st
-import pandas as pd
+import easyocr
 from PIL import Image
+import numpy as np
 
-st.title("LINE訂餐整理工具 V1.1")
+st.title("LINE訂餐 OCR 測試")
 
-uploaded_files = st.file_uploader(
-    "上傳 LINE 截圖",
-    type=["jpg", "jpeg", "png"],
-    accept_multiple_files=True
+uploaded_file = st.file_uploader(
+    "上傳圖片",
+    type=["jpg", "jpeg", "png"]
 )
 
-if uploaded_files:
+if uploaded_file:
 
-    st.success(
-        f"已上傳 {len(uploaded_files)} 張圖片"
-    )
+    image = Image.open(uploaded_file)
 
-    for file in uploaded_files:
-        image = Image.open(file)
+    st.image(image)
 
-        st.image(
-            image,
-            caption=file.name,
-            use_container_width=True
+    if st.button("開始 OCR"):
+
+        reader = easyocr.Reader(
+            ['ch_tra', 'en']
         )
 
-    if st.button("開始辨識"):
-
-        st.info("OCR功能準備中")
-
-        data = [
-            ["黃東源", "香酥排骨餐盒", 100],
-            ["鄭庭宜", "滷雞腿餐盒", 115]
-        ]
-
-        df = pd.DataFrame(
-            data,
-            columns=["姓名", "餐點", "金額"]
+        results = reader.readtext(
+            np.array(image),
+            detail=0
         )
 
-        st.dataframe(df)
+        st.write(results)

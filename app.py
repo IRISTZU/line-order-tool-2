19bf2a873f92215e859466dcd12c720d726bad1c
IRISTZU <mlive4h@gmail.com>
2026-08-05 16:26:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,39 @@
 import streamlit as st
-import pandas as pd
+import requests
 from PIL import Image
-import io
 
-st.set_page_config(page_title="LINE訂餐整理工具")
+API_KEY = "你的APIKEY"
 
-st.title("LINE訂餐整理工具")
+st.title("LINE訂餐OCR")
 
-uploaded_files = st.file_uploader(
-    "上傳 LINE 截圖",
-    type=["jpg", "jpeg", "png"],
+files = st.file_uploader(
+    "上傳圖片",
     accept_multiple_files=True
 )
 
-if uploaded_files:
+if files:
 
-    st.success(f"已上傳 {len(uploaded_files)} 張圖片")
+    if st.button("開始辨識"):
 
-    st.subheader("圖片預覽")
+        for file in files:
 
-    for file in uploaded_files:
-        image = Image.open(file)
-        st.image(image, caption=file.name)
-
-    st.subheader("訂單資料")
-
-    sample_data = [
-        ["黃東源", "香酥排骨餐盒", 1, 100],
-        ["鄭庭宜", "滷雞腿餐盒", 1, 115]
-    ]
-
-    df = pd.DataFrame(
-        sample_data,
-        columns=["姓名", "餐點", "數量", "金額"]
-    )
-
-    edited_df = st.data_editor(
-        df,
-        use_container_width=True,
-        num_rows="dynamic"
-    )
-
-    if st.button("產生Excel"):
-
-        output = io.BytesIO()
-
-        with pd.ExcelWriter(
-            output,
-            engine="openpyxl"
-        ) as writer:
-
-            edited_df.to_excel(
-                writer,
-                sheet_name="訂單明細",
-                index=False
+            response = requests.post(
+                "https://api.ocr.space/parse/image",
+                files={"filename": file},
+                data={
+                    "apikey": API_KEY,
+                    "language": "cht"
+                }
             )
 
-            summary = (
-                edited_df.groupby("餐點")["數量"]
-                .sum()
-                .reset_index()
-            )
+            result = response.json()
 
-            summary.to_excel(
-                writer,
-                sheet_name="餐點統計",
-                index=False
-            )
+            try:
+                text = result["ParsedResults"][0]["ParsedText"]
 
-        output.seek(0)
+                st.subheader(file.name)
 
-        st.download_button(
-            "下載Excel",
-            data=output,
-            file_name="LINE訂餐統計.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
+                st.text(text)
+
+            except:
+                st.error(f"{file.name} 辨識失敗")
